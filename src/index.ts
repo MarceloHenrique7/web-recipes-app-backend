96d278b7de2abcbdc0ba11f18cf0c296ba9e5c87
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-07-14 20:45:27 -0300

    parsing cors options

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,13 @@ dotenv.config();
 import { PrismaClient } from '@prisma/client';
 const app = express();
 
-app.use(cors())
+
+const corsOptions = {
+    origin: 'https://web-app-recipes-frontend.onrender.com',
+    optionsSuccessStatus: 200,
+  };
+
+app.use(cors(corsOptions))
 
 app.use(bodyParser.raw({ type: 'application/json' }))
 app.use(express.json())
