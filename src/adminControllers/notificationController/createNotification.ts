f22a08b35876e4de99ea0eeafb9ab1a56c1caa11
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-07-28 15:50:09 -0300

    att

--- src/adminControllers/notificationController/createNotification.ts
+++ src/adminControllers/notificationController/createNotification.ts
@@ -1,59 +1,34 @@
-import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
-import { prisma } from "../../index";
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import { prisma } from "../../index"
 
 export const createNotification = async (req: Request, res: Response) => {
     try {
-        const { userId, recipientUserId, recipeId, type, title, subtitle, description } = req.body;
-
-        const userToSave = await prisma.user.findUnique({ where: { id: userId } });
-        const userSendNotification = await prisma.user.findUnique({ where: { id: recipientUserId } });
-        const recipeSaved = await prisma.recipe.findUnique({ where: { id: recipeId } });
-
+        const { userId, recipientUserId, recipeId, type, subtitle, description } = req.body
+        const userToSave = await prisma.user.findUnique( { where: { id: userId }  } )
+        const userSendNotification = await prisma.user.findUnique( { where: { id: recipientUserId }  } )
+        const recipeSaved = await prisma.recipe.findUnique( { where: { id: recipeId }  } )
+        
         if (!userToSave || !userSendNotification) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: "User not Found" });
+            return res.status(StatusCodes.NOT_FOUND).json({ message: "User not Found" })
         }
 
         if (!recipeSaved) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: "Recipe not Found" });
+            return res.status(StatusCodes.NOT_FOUND).json({ message: "Recipe not Found" })
         }
 
-        if (type === 'SELL') {
+        if ( type === 'SELL' ) {
             const notification = await prisma.notification.create({
-                data: {
-                    userId,
-                    recipientUserId,
-                    recipeId,
-                    type,
-                    title,
-                    subtitle,
-                    description,
-                    createdAt: new Date(),
-                    isRead: false,
-                    isGeneral: false,
-                    readByUsers: []
-                }
-            });
-            return res.status(StatusCodes.CREATED).json(notification);
+                data: req.body
+            })
+            return res.status(StatusCodes.CREATED).json(notification)
         }
+        if ( type === 'SAVE' ) {
 
-        if (type === 'SAVE') {
             const notification = await prisma.notification.create({
-                data: {
-                    userId,
-                    recipientUserId,
-                    recipeId,
-                    type,
-                    title,
-                    subtitle,
-                    description,
-                    createdAt: new Date(),
-                    isRead: false,
-                    isGeneral: false,
-                    readByUsers: []
-                }
-            });
-
+                data: req.body
+            })
+            
             await prisma.user.update({
                 where: { id: userId },
                 data: {
@@ -61,13 +36,13 @@ export const createNotification = async (req: Request, res: Response) => {
                         push: recipeId
                     }
                 }
-            });
-
-            return res.status(StatusCodes.CREATED).json(notification);
+            })
+            return res.status(StatusCodes.CREATED).json(notification)
         }
 
     } catch (error) {
-        console.log(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Something Went Wrong" });
+        console.log(error)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Something Went Wrong" })
     }
-};
+}
+
